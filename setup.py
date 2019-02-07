bb62a1789aa8f79d3e3982a484913558d81c9f83
Joachim Moeyens <moeyensj@gmail.com>
2019-02-06 18:05:58 -0800

    Initial packaging attempt

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+   name="difi",
+   version="0.0.1.dev0",
+   license="BSD 3-Clause License",
+   author="Joachim Moeyens",
+   author_email="moeyensj@uw.edu",
+   long_description=open("README.md").read(),
+   long_description_content_type="text/markdown",
+   install_requires=[
+       'numpy',
+       'pandas',
+       'pytest',
+       'pytest-cov'
+   ],
+   url="https://github.com/moeyensj/difi",
+   packages=["difi"],
+   package_dir={"difi": "difi"},
+   package_data={"difi": ["tests/*.txt"]},
+   setup_requires=["pytest-runner"],
+   tests_require=["pytest"],
+)
