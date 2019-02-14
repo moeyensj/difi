1049e720769d5142e4b6112f6ca4ca78732e077c
Joachim Moeyens <moeyensj@gmail.com>
2019-02-14 13:45:47 -0800

    Added two more test cases to test_io

--- difi/tests/test_io.py
+++ difi/tests/test_io.py
@@ -12,6 +12,7 @@ def test_readLinkagesByLineFile_fromFile():
     # Load solution
     linkageMembers_solution = pd.read_csv(os.path.join(os.path.dirname(__file__), "linkageMembers_solution.txt"), sep=" ", index_col=False, dtype={"obs_id" : str, "linkage_id": np.int64})
     
+    # Case 1a: Test file comparison
     linkageMembers_test = readLinkagesByLineFile(linkagesFile,
                                                  readCSVKwargs={"header": None},
                                                  linkageIDStart=1,
@@ -24,3 +25,35 @@ def test_readLinkagesByLineFile_fromFile():
     
     # Assert dataframes are equal
     assert_frame_equal(linkageMembers_test, linkageMembers_solution)
+    
+    # Case 1b: Test different column names
+    linkageMembers_test = readLinkagesByLineFile(linkagesFile,
+                                                 readCSVKwargs={"header": None},
+                                                 linkageIDStart=1,
+                                                 columnMapping={"obs_id": "obsID",
+                                                                "linkage_id" : "linkageID"})
+    
+    # Re-arange columns in case order is changed (python 3.5 and earlier)
+    linkageMembers_test = linkageMembers_test[["linkageID", 
+                                               "obsID"]]
+    linkageMembers_solution.rename(columns={"obs_id": "obsID", "linkage_id" : "linkageID"}, inplace=True)
+    
+    # Assert dataframes are equal
+    assert_frame_equal(linkageMembers_test, linkageMembers_solution)
+    
+    # Case 1c: Test different linkage IDs
+    linkageMembers_test = readLinkagesByLineFile(linkagesFile,
+                                                 readCSVKwargs={"header": None},
+                                                 linkageIDStart=21311232,
+                                                 columnMapping={"obs_id": "obsID",
+                                                                "linkage_id" : "linkageID"})
+    
+    # Re-arange columns in case order is changed (python 3.5 and earlier)
+    linkageMembers_test = linkageMembers_test[["linkageID", 
+                                               "obsID"]]
+    linkageMembers_solution.rename(columns={"obs_id": "obsID", "linkage_id" : "linkageID"}, inplace=True)
+    linkageMembers_solution["linkageID"] += 21311232
+    
+    # Assert dataframes are equal
+    assert_frame_equal(linkageMembers_test, linkageMembers_solution)
+
