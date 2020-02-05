04faf67eea1100545b8c77903c7a7e12d9516e8e
Joachim Moeyens <moeyensj@gmail.com>
2020-02-05 14:07:46 -0800

    Correct Pandas assert_frame_equal import

--- difi/tests/test_difi.py
+++ difi/tests/test_difi.py
@@ -5,7 +5,7 @@ import random
 import pytest
 import numpy as np
 import pandas as pd 
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 from difi import analyzeLinkages
 
--- difi/tests/test_io.py
+++ difi/tests/test_io.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd 
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 from difi import readLinkagesByLineFile
 
