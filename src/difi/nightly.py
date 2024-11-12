7528c92f1bd48847e76b2760624da0631c41679e
Joachim Moeyens <moeyensj@gmail.com>
2024-11-12 13:28:13 -0800

    Add NightlyOrbitSummary

---
+++ src/difi/nightly.py
@@ -0,0 +1,178 @@
+import numpy as np
+import pyarrow as pa
+import pyarrow.compute as pc
+import quivr as qv
+from adam_core.orbit_determination import FittedOrbitMembers
+
+from difi.observations import Observations
+
+from .metrics import haversine_distance
+
+
+class NightlyOrbitSummary(qv.Table):
+    #: Orbit ID
+    orbit_id = qv.LargeStringColumn()
+    #: Night
+    night = qv.Int64Column()
+    #: Number of observations for this night
+    num_obs = qv.Int64Column()
+    #: Number of unique filters
+    num_filters = qv.Int64Column()
+    #: Time span of the observations in seconds
+    dtime = qv.Float64Column()
+    #: Right ascension difference between the first and last observation in arcseconds
+    dra = qv.Float64Column()
+    #: Declination difference between the first and last observation in arcseconds
+    ddec = qv.Float64Column()
+    #: Sky distance between the first and last observation in arcseconds
+    dsky = qv.Float64Column()
+    #: Sky velocity between the first and last observation in arcseconds per second
+    vsky = qv.Float64Column()
+    #: Magnitude difference between the brightest and faintest observation
+    dmag = qv.Float64Column()
+    #: Magnitude mean of the observations
+    mag_mean = qv.Float64Column()
+    #: Magnitude standard deviation of the observations
+    mag_sigma = qv.Float64Column()
+    #: Sum of the chi2 values of the residuals
+    chi2_sum = qv.Float64Column(nullable=True)
+    #: Mean of the chi2 values of the residuals
+    chi2_mean = qv.Float64Column(nullable=True)
+    #: Standard deviation of the chi2 values of the residuals
+    chi2_sigma = qv.Float64Column(nullable=True)
+    #: Difference between the maximum and minimum chi2 values of the residuals
+    dchi2 = qv.Float64Column(nullable=True)
+
+    @classmethod
+    def create(cls, orbit_members: FittedOrbitMembers, observations: Observations) -> "NightlyOrbitSummary":
+        """
+        Create a nightly summary for the given orbit members and observations.
+
+        Parameters
+        ----------
+        orbit_members : FittedOrbitMembers
+            The fitted orbit members.
+        observations : Observations
+            The observations table.
+
+        Returns
+        -------
+        NightlyOrbitSummary
+            The nightly summary table.
+        """
+        assert pc.all(pc.is_in(orbit_members.obs_id.unique(), observations.id)).as_py()
+
+        members_table = orbit_members.flattened_table().drop_columns(["residuals.values"])
+        members_table = members_table.join(
+            observations.flattened_table()
+            .append_column("mjd", observations.time.mjd())
+            .select(
+                [
+                    "id",
+                    "night",
+                    "mjd",
+                    "ra",
+                    "dec",
+                    "mag",
+                    "filter",
+                ]
+            ),
+            "obs_id",
+            "id",
+        )
+
+        members_table = members_table.sort_by(
+            [("orbit_id", "ascending"), ("night", "ascending"), ("mjd", "ascending")]
+        )
+
+        nightly_summary = members_table.group_by(["orbit_id", "night"], use_threads=False).aggregate(
+            [
+                ("mjd", "min"),
+                ("mjd", "max"),
+                ("night", "count"),
+                ("ra", "first"),
+                ("ra", "last"),
+                ("dec", "first"),
+                ("dec", "last"),
+                ("dec", "mean"),
+                ("mag", "max"),
+                ("mag", "min"),
+                ("mag", "mean"),
+                ("mag", "stddev"),
+                ("residuals.chi2", "min"),
+                ("residuals.chi2", "max"),
+                ("residuals.chi2", "mean"),
+                ("residuals.chi2", "stddev"),
+                ("residuals.chi2", "sum"),
+                ("filter", "count_distinct"),
+            ]
+        )
+
+        nightly_summary = nightly_summary.append_column(
+            "dtime",
+            pa.array(
+                86400.0
+                * (
+                    nightly_summary["mjd_max"].to_numpy(zero_copy_only=False)
+                    - nightly_summary["mjd_min"].to_numpy(zero_copy_only=False)
+                )
+            ),
+        )
+
+        ra1 = nightly_summary["ra_first"].to_numpy(zero_copy_only=False)
+        ra2 = nightly_summary["ra_last"].to_numpy(zero_copy_only=False)
+        dec1 = nightly_summary["dec_first"].to_numpy(zero_copy_only=False)
+        dec2 = nightly_summary["dec_last"].to_numpy(zero_copy_only=False)
+        dtime = nightly_summary["dtime"].to_numpy(zero_copy_only=False)
+
+        dra = np.where(ra2 - ra1 > 180, ra2 - ra1 - 360, ra2 - ra1)
+        ddec = dec2 - dec1
+
+        # Compute the haversine distance between the first and last observation
+        # for each orbit-night pair
+        dsky = haversine_distance(ra1, dec1, ra2, dec2)
+        vsky = dsky / dtime
+        vsky = np.where(np.isnan(vsky), 0.0, vsky)
+        nightly_summary = nightly_summary.append_column(
+            "dsky",
+            pa.array(dsky),
+        )
+        nightly_summary = nightly_summary.append_column(
+            "vsky",
+            pa.array(vsky),
+        )
+        nightly_summary = nightly_summary.append_column(
+            "dra",
+            pa.array(dra),
+        )
+        nightly_summary = nightly_summary.append_column(
+            "ddec",
+            pa.array(ddec),
+        )
+        nightly_summary = nightly_summary.append_column(
+            "dmag", pc.subtract(nightly_summary["mag_max"], nightly_summary["mag_min"])
+        )
+        nightly_summary = nightly_summary.append_column(
+            "dchi2", pc.subtract(nightly_summary["residuals.chi2_max"], nightly_summary["residuals.chi2_min"])
+        )
+
+        nightly_summary = nightly_summary.sort_by([("orbit_id", "ascending"), ("night", "ascending")])
+
+        return NightlyOrbitSummary.from_kwargs(
+            orbit_id=nightly_summary["orbit_id"],
+            night=nightly_summary["night"],
+            num_obs=nightly_summary["night_count"],
+            num_filters=nightly_summary["filter_count_distinct"],
+            dtime=nightly_summary["dtime"],
+            dra=pc.multiply(nightly_summary["dra"], 3600.0),
+            ddec=pc.multiply(nightly_summary["ddec"], 3600.0),
+            dsky=pc.multiply(nightly_summary["dsky"], 3600.0),
+            vsky=pc.multiply(nightly_summary["vsky"], 3600.0),
+            dmag=nightly_summary["dmag"],
+            mag_mean=nightly_summary["mag_mean"],
+            mag_sigma=nightly_summary["mag_stddev"],
+            chi2_sum=nightly_summary["residuals.chi2_sum"],
+            chi2_mean=nightly_summary["residuals.chi2_mean"],
+            chi2_sigma=nightly_summary["residuals.chi2_stddev"],
+            dchi2=nightly_summary["dchi2"],
+        )
---
+++ src/difi/tests/test_nightly.py
@@ -0,0 +1,107 @@
+import numpy as np
+from adam_core.coordinates import Residuals
+from adam_core.orbit_determination import FittedOrbitMembers
+from adam_core.time import Timestamp
+
+from ..nightly import NightlyOrbitSummary
+from ..observations import Observations
+
+
+def test_NightlyOrbitSummary_create():
+    # Test that the NightlyOrbitSummary.create method to group
+    # orbit members by orbit and night and calculate summary statistics correctly
+    times = Timestamp.from_kwargs(
+        days=[59000, 59000, 59001, 59001, 59002, 59003], nanos=[0, 60 * 1e9, 0, 60 * 1e9, 0, 0], scale="utc"
+    )
+
+    observations = Observations.from_kwargs(
+        id=["1", "2", "3", "4", "5", "6"],
+        time=times,
+        ra=[10, 10.5, 11, 11.5, 12, 12.5],
+        dec=[20, 20.1, 21, 21.1, 22, 23],
+        mag=[22, 22.1, 22.8, 22.9, 23, 22.9],
+        filter=["g", "r", "g", "g", "r", "z"],
+        observatory_code=["X05", "X05", "I41", "X05", "X05", "X05"],
+        night=times.days,
+    )
+
+    orbit_members = FittedOrbitMembers.from_kwargs(
+        orbit_id=["1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2", "2"],
+        obs_id=["1", "2", "3", "4", "5", "6", "1", "2", "3", "4", "5", "6"],
+        residuals=Residuals.from_kwargs(
+            values=[
+                [np.nan, 0.002, 0.002, np.nan, np.nan, np.nan],
+                [np.nan, 0.002, 0.002, np.nan, np.nan, np.nan],
+                [np.nan, 0.002, 0.002, np.nan, np.nan, np.nan],
+                [np.nan, 0.002, 0.002, np.nan, np.nan, np.nan],
+                [np.nan, 0.002, 0.002, np.nan, np.nan, np.nan],
+                [np.nan, 0.002, 0.002, np.nan, np.nan, np.nan],
+                [np.nan, 0.004, 0.004, np.nan, np.nan, np.nan],
+                [np.nan, 0.006, 0.006, np.nan, np.nan, np.nan],
+                [np.nan, 0.008, 0.008, np.nan, np.nan, np.nan],
+                [np.nan, 0.010, 0.010, np.nan, np.nan, np.nan],
+                [np.nan, 0.012, 0.012, np.nan, np.nan, np.nan],
+                [np.nan, 0.014, 0.014, np.nan, np.nan, np.nan],
+            ],
+            chi2=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+        ),
+    )
+
+    nightly_summary = NightlyOrbitSummary.create(orbit_members, observations)
+
+    assert nightly_summary.orbit_id.to_pylist() == ["1", "1", "1", "1", "2", "2", "2", "2"]
+    assert nightly_summary.night.to_pylist() == [59000, 59001, 59002, 59003, 59000, 59001, 59002, 59003]
+    assert nightly_summary.num_obs.to_pylist() == [2, 2, 1, 1, 2, 2, 1, 1]
+    assert nightly_summary.num_filters.to_pylist() == [2, 1, 1, 1, 2, 1, 1, 1]
+    np.testing.assert_almost_equal(
+        nightly_summary.dtime.to_numpy(zero_copy_only=False),
+        [60, 60, 0, 0, 60, 60, 0, 0],
+        decimal=6,
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.dra.to_numpy(zero_copy_only=False),
+        [1800.0, 1800.0, 0.0, 0.0, 1800.0, 1800.0, 0.0, 0.0],
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.ddec.to_numpy(zero_copy_only=False), [360.0, 360.0, 0.0, 0.0, 360.0, 360.0, 0.0, 0.0]
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.dsky.to_numpy(zero_copy_only=False),
+        [1728.8058078, 1718.0213960, 0.0, 0.0, 1728.8058078, 1718.0213960, 0.0, 0.0],
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.vsky.to_numpy(zero_copy_only=False),
+        [1728.8058078 / 60, 1718.0213960 / 60, 0.0, 0.0, 1728.8058078 / 60, 1718.0213960 / 60, 0.0, 0.0],
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.dmag.to_numpy(zero_copy_only=False),
+        [
+            0.1,
+            0.1,
+            0.0,
+            0.0,
+            0.1,
+            0.1,
+            0.0,
+            0.0,
+        ],
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.mag_mean.to_numpy(zero_copy_only=False),
+        [22.05, 22.85, 23.0, 22.9, 22.05, 22.85, 23.0, 22.9],
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.mag_sigma.to_numpy(zero_copy_only=False), [0.05, 0.05, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0]
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.chi2_sum.to_numpy(zero_copy_only=False), [2.0, 2.0, 1.0, 1.0, 3.0, 7.0, 5.0, 6.0]
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.chi2_mean.to_numpy(zero_copy_only=False), [1.0, 1.0, 1.0, 1.0, 1.5, 3.5, 5.0, 6.0]
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.chi2_sigma.to_numpy(zero_copy_only=False), [0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0]
+    )
+    np.testing.assert_almost_equal(
+        nightly_summary.dchi2.to_numpy(zero_copy_only=False), [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0]
+    )
